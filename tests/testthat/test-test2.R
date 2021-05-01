15f366e53febc481da160a716693e548d31ce975
liusf15 <sfliu@stanford.edu>
2021-04-30 18:05:41 -0700

    cleanup

--- tests/testthat/test-test2.R
+++
@@ -1,3 +0,0 @@
-test_that("median", {
-  expect_equal(test2(c(1, 4, 5)), 4)
-})
--- tests/testthat/test-test_fun.R
+++
@@ -1,4 +0,0 @@
-test_that("multiplication works", {
-  expect_equal(test_fun(3), 4)
-  expect_identical(test_fun(2), 3)
-})
